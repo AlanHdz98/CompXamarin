21eed7d01b3ccdda51b5654eb89b178538b5611c
alan hernandez hernandez <ukog_nala@hotmail.com>
2022-02-13 22:18:14 -0600

    Agregar archivos de proyecto.

---
+++ CompXamarin/Vistas/AlertaN1.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rg.Plugins.Popup;
+using Rg.Plugins.Popup.Pages;
+using Rg.Plugins.Popup.Services;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CompXamarin.Vistas
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AlertaN1 : PopupPage
+    {
+        public AlertaN1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOkay_Clicked(object sender, EventArgs e)
+        {
+            PopupNavigation.Instance.PopAsync();
+        }
+    }
+}
---
+++ CompXamarin/Vistas/MenuPrincipal.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CompXamarin.Modelo;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CompXamarin.Vistas
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MenuPrincipal : ContentPage
+    {
+        public IList<Mcomidas> Comida { get; private set;}
+
+        public MenuPrincipal()
+        {
+            InitializeComponent();
+
+            Comida = new List<Mcomidas>();
+            Comida.Add(new Mcomidas
+            {
+                Nombre = "Imagen circular",
+                Imagen = "emoji.png"
+            });
+            Comida.Add(new Mcomidas
+            {
+                Nombre = "Alertas",
+                Imagen = "alerta.png"
+            });
+            Comida.Add(new Mcomidas
+            {
+                Nombre = "Tarjetas",
+                Imagen = "tarjeta.png"
+            });
+            Comida.Add(new Mcomidas
+            {
+                Nombre = "Fechas",
+                Imagen = "fecha.png"
+            });
+
+            BindingContext = this;
+
+        }
+
+        private void ListView_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            Mcomidas seleccion = e.Item as Mcomidas;
+            if(seleccion.Nombre == "Tarjetas")
+            {
+                Navigation.PushAsync(new Tarjetas());
+            }
+            if (seleccion.Nombre == "Alertas")
+            {
+                Navigation.PushAsync(new Alertas());
+            }
+            if (seleccion.Nombre == "Imagen circular")
+            {
+                Navigation.PushAsync(new circularImagen());
+            }
+            if (seleccion.Nombre == "Fechas")
+            {
+                Navigation.PushAsync(new fechaHora());
+            }
+        }
+    }
+}
